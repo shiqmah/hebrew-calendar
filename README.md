02d6d18d09113c6fe2609dadd7a5d06093ae1234
shiqmah <shiqmah@protonmail.com>
2018-12-15 21:49:02 +0200

    Start adding some constants

--- README.md
+++ README.md
@@ -2,7 +2,6 @@
 
 [![Build Status](https://travis-ci.org/shiqmah/hebrew-calendar.svg)](https://travis-ci.org/shiqmah/hebrew-calendar)
 [![Crates.io](https://img.shields.io/crates/v/hebrew-calendar.svg)](https://crates.io/crates/hebrew-calendar)
-[![codecov](https://codecov.io/gh/shiqmah/hebrew-calendar/branch/master/graph/badge.svg)](https://codecov.io/gh/shiqmah/hebrew-calendar)
 [![docs.rs](https://docs.rs/hebrew-calendar/badge.svg)](https://docs.rs/hebrew-calendar/)
 
 A Hebrew Calendar Library in Rust
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,22 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
+const NISAN: u8 = 1;
+const IYYAR: u8 = 2;
+const SIVAN: u8 = 3;
+const TAMMUZ: u8 = 4;
+const AV: u8 = 5;
+const ELUL: u8 = 6;
+const TISHRI: u8 = 7;
+const MARHESHVAN: u8 = 8;
+const KISLEV: u8 = 9;
+const TEVET: u8 = 10;
+const SHEVAT: u8 = 11;
+const ADAR: u8 = 12;
+const ADARII: u8 = 13;
+
+// fn fixed_from_julian
+// fn julian_date
+
+// const hebrew_epoch
+
+fn hebrew_date(year: u16, month: u8, day: u8) -> (u16, u8, u8) {
+    (year, month, day)
 }
